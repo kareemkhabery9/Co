a83c4e40fd0526fb9f8ac81e45eb75e2ab2a2ede
kareemkhabery9 <kareemkhabery6@gmail.com>
2025-08-17 06:52:46 +0300

    Q1

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,6 +5,36 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+            #region Q1
+
+            //This is a simple C# program that prints "Hello, World!" to the console.
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
